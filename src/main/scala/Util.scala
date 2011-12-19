8a3957a93ed605977d720afb3259fbbaf6496429
Lars Hupel <lars.hupel@mytum.de>
2011-12-19 22:02:42 +0000

    really simple logging

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -1,5 +1,8 @@
 package edu.tum.cs.afl
 
+import annotation.elidable
+import elidable._
+
 import scalaz._
 import Scalaz._
 
@@ -12,5 +15,8 @@ object Util {
 			(0 /: seq) { case (acc, elem) => (elem ? 1 | 0) + 2 * acc }
 
 	def wordToIntWord(word: Seq[Seq[Boolean]]) = (word map seqToInt).mkString("(", ",", ")") 
+
+	@elidable(FINE)
+	def log(x: Any) = Console.err println x
 
 }
