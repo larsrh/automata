a994fe45d04bfb0c6230168a87e8cf93b0fa2c94
Lars Hupel <lars.hupel@mytum.de>
2011-12-21 16:44:41 +0000

    add timing and documentation to launcher

--- launch.sh
+++ launch.sh
@@ -11,24 +11,30 @@ cd "$(dirname "$0")" || die "Could not change directory"
 
 if [ ! -e "target/afl-assignment-assembly-0.1.jar" ]; then
 	echo "*** Build process starting"
-	echo "*** Please measure only *subsequent* runs of this program."
+	echo "*** The runtime is measured automatically after the build process has completed."
 	echo
-	echo "*** [1/3] Please wait, the build tool is frobnicating the flux capacitor."
+	echo "*** [1/4] Please wait, the build tool is frobnicating the flux capacitor."
 	echo "*** Depending on your internet connection, this might take a while."
 	echo
 	./sbt update || die "Download failed"
 	echo
 	echo "*** Update successful."
-	echo "*** [2/3] Please wait, the build tool is building."
+	echo "*** [2/4] Please wait, the build tool is building."
 	echo
 	./sbt compile || die "Build failed"
 	echo
 	echo "*** Build successful."
-	echo "*** [3/3] Please wait, the build tool is making an awesome package."
+	echo "*** [3/4] Please wait, the build tool is making an awesome package."
 	echo
 	./sbt assembly || die "Packaging failed"
 	echo
-	echo "*** Completed."
+	echo "*** Packaging successful."
+	echo "*** [4/4] Please wait, the build tool makes up some documentation."
+	echo
+	./sbt doc || die "Documentation failed"
+	echo
+	echo "*** Documentation successful."
+	echo "*** Build process completed."
 else
 	echo "*** Lucky you, there's already a package."
 	echo
@@ -39,7 +45,7 @@ cd "$OLDDIR" || die "Could not change directory"
 echo "*** Processing files $@"
 echo
 
-java -jar "$(dirname "$0")/target/afl-assignment-assembly-0.1.jar" "$@"
+time java -jar "$(dirname "$0")/target/afl-assignment-assembly-0.1.jar" "$@"
 
 echo
 echo "*** Done."
