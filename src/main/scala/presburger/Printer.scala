455d62c1bd5d6ef244c5d41689e0c3b5ae4b9972
Lars Hupel <lars.hupel@mytum.de>
2012-01-30 00:44:11 +0000

    fix automatonToDotty

--- src/main/scala/presburger/Printer.scala
+++ src/main/scala/presburger/Printer.scala
@@ -12,26 +12,32 @@ import edu.tum.cs.afl.Util._
 object Printer {
 
 	def automatonToDotty(automaton: Automaton): List[String] = {
-		def aux(state: Automaton): List[String] = {
-			if (state == state.automaton.EmptySet)
+		val visited = mutable.Set[Automaton]()
+
+		def aux(state: Automaton): List[String] =
+			if (visited contains state)
 				Nil
-			else if (state == state.automaton.Epsilon)
-				List(state.id + "[peripheries=2];")
-			else
-				state.asInstanceOf[MasterAutomaton#Succ].succs zip chars(automaton.automaton.dimension) flatMap { case (s, c) =>
-					if (s.empty)
-						Nil
-					else {
-						val head = state.id + " -> " + s.id + " [label=\"" + (c map { elem => elem ? '1' | '0' } mkString "")  +  " \"];"
-						val tail = aux(s)
-						head :: tail
+			else {
+				visited += state
+
+				if (state == state.automaton.EmptySet || state == state.automaton.Epsilon)
+					Nil
+				else
+					state.asInstanceOf[MasterAutomaton#Succ].succs zip chars(automaton.automaton.dimension) flatMap { case (s, c) =>
+						if (s.empty)
+							Nil
+						else {
+							val head = state.id + " -> " + s.id + " [label=\"" + (c map { elem => elem ? '1' | '0' } mkString "")  +  " \"];"
+							val tail = aux(s)
+							head :: tail
+						}
 					}
-				}
-		}
+			}
 
 		val init = automaton.id + "[shape=diamond];"
+		val end = if (!automaton.empty) "1[peripheries=2];" else ""
 
-		"digraph G {" :: init :: (aux(automaton) :+ "}")
+		"digraph G {" :: init :: end :: (aux(automaton) :+ "}")
 	}
 
 }
